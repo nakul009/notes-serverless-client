d90afba46209d193accfcc92007837788edb4510
nakul009 <sahayatanakul2867@gmail.com>
2020-04-23 15:58:31 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-nakul"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://jwq7i48jl0.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_ExIUsfikU",
+        APP_CLIENT_ID: "h4op98udph2ap6o89tnpv50ro",
+        IDENTITY_POOL_ID: "us-east-1:796266cc-4ade-448c-8e84-c79b1e7de840"
+    }
+};
